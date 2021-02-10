593fe786dcea87bd00e25179306c1cc1d950e68b
westlag114 <fsww2353@gmail.com>
2021-02-10 16:24:44 +0900

    コメント追加しました

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
+// コメント追加
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
